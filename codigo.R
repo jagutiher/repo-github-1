d76a333871115200541ada6b365378eb89fbfcb9
jgutiher <88879355+jagutiher@users.noreply.github.com>
2023-04-18 11:30:41 +0200

    Add files via upload

---
+++ codigo.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+isciii <- read_csv("https://cnecovid.isciii.es/covid19/resources/casos_hosp_uci_def_sexo_edad_provres.csv",
+                   na = "NC")
+
+isciii_m <- 
+  isciii |> 
+  drop_na( sexo) |> 
+  filter( provincia_iso == "M",
+          fecha <= ymd("2020-12-31")) |> 
+  select( fecha, sexo, grupo_edad, num_casos) |> 
+  summarise( num_casos = sum(num_casos), 
+             .by = c(fecha, sexo))
+
+write_csv( isciii_m, "./datos_madrid.csv")
